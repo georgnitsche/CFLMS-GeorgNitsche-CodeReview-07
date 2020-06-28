a849a1644c9fb4222bef84fe7a43be58b3179f3d
georgnitsche <georg.nitsche@alumni.uni-graz.at>
2020-06-28 17:46:32 +0200

    hero section und navbar fertig

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-hero',
+  selector: 'hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
-
+heroHeading: string;
+heroText: string;
+heroBtnText: string;
+heroBtnUrl: string;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
